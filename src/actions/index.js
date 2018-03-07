a46f2ff2257a101335d62f8b8acca37fee8d844a
Eddie Kim <jungyupkim4@gmail.com>
2018-03-07 14:54:26 -0500

    Third Commit
    added actions folder with action creator with promise-redux and axios library.
    also added new reducer file and container file.

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '601d3f1c7a43d7f8485d567cc56a839a';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
